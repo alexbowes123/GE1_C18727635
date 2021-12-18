f01681fa1313ec02596024fb531055a6bb371bac
alexbowes123 <alexbowes1@hotmail.com>
2021-12-18 17:29:38 +0000

    add iconosphere and start on color lerp

---
+++ C18727635 GE1 Assignment/Assets/Scripts/LerpColor.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class LerpColor : MonoBehaviour
+{
+    MeshRenderer iconMeshRenderer;
+
+    [SerializeField] [Range(0f, 1f)] float lerpTime;
+
+    [SerializeField] Color[] myColors;
+
+    int colorIndex = 0;
+    int len;
+
+    float t = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iconMeshRenderer = GetComponent <MeshRenderer>();
+        len = myColors.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        iconMeshRenderer.material.color = Color.Lerp(iconMeshRenderer.material.color, myColors[colorIndex], lerpTime*Time.deltaTime);
+
+        t = Mathf.Lerp(t, 1f, lerpTime*Time.deltaTime);
+        if (t > .9f){
+            t = 0f;
+            colorIndex++;
+            colorIndex = (colorIndex >= len) ? 0 : colorIndex;
+        }
+    }
+}
